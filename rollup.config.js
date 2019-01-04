cb58891ea447b315a52a596bdb10d2e43bf19727
Preet Shihn <preetshihn@gmail.com>
2019-01-04 15:31:50 -0800

    .

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import { terser } from "rollup-plugin-terser";
+
+const input = 'index.js';
+
+export default [
+  {
+    input,
+    output: {
+      file: `dist/workly.js`,
+      format: 'iife',
+      name: 'workly'
+    },
+    plugins: [terser()]
+  },
+  {
+    input,
+    output: {
+      file: `dist/workly.m.js`,
+      format: 'esm'
+    },
+    plugins: [terser()]
+  },
+];
